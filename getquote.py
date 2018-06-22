62c6a91657f00e0eb6712be44f8666ab863b428f
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2018-06-22 18:13:08 +0200

    Add currency support and fix google finance api endpoint

--- getquote.py
+++ getquote.py
@@ -6,26 +6,28 @@ from json import loads
 
 # prices api
 CRYPTO_API = 'https://api.coinmarketcap.com/v1/ticker/'
-STANDARD_API = 'https://www.google.com/finance/info?q='
+STANDARD_API = 'https://finance.google.com/finance?output=json&q='
 
 # crypto symbols
-CRYPTO_SYMBOLS = {'btc':'bitcoin', 'ltc':'litecoin', 'eth':'ethereum'}
+CRYPTO_SYMBOLS = {'btc':'bitcoin', 'ltc':'litecoin', 'eth':'ethereum', 'iot':'iota'}
 
-def getCrypto(symbol):
+def getCrypto(symbol, currency):
     """Pull data for cryptocurrencies
     """
 
     try:
-        price = request('get', '{}{}/'.format(CRYPTO_API, CRYPTO_SYMBOLS[symbol]))
+        adress = '{}{}/?convert={}'.format(CRYPTO_API, CRYPTO_SYMBOLS[symbol], currency)
+        price = request('get', adress)
         json_price = loads(price.text)
 
     except:
         sys.exit(0)
 
-    price = json_price[0]['price_usd']
+    price_lookup = 'price_' + currency.lower()
+    price = json_price[0][price_lookup]
     current_time = time.strftime('%Y/%m/%d %X')
 
-    return '{} {} ${}'.format(current_time, symbol, price)
+    return '{} {} {} {}'.format(current_time, symbol, currency, price)
 
 def getStandard(symbol):
     """Pull data for standard symbols
@@ -44,11 +46,12 @@ def getStandard(symbol):
     return '{} {} ${}'.format(current_time, symbol, price)
 
 if __name__ == '__main__':
+    currency = sys.argv[2].upper() if len(sys.argv) > 2 else 'EUR' 
     
     # check if symbol is a cryptocurrency
     if sys.argv[1].lower() in CRYPTO_SYMBOLS:
-        print(getCrypto(sys.argv[1].lower()))
+        print(getCrypto(sys.argv[1].lower(), currency))
 
     # everything else, send to the standard api
     else:
-        print(getStandard(sys.argv[1]))
+        print(getStandard(sys.argv[1], currency))
